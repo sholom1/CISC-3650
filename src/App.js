4b13a7ab64c6ce3e0b299852bbdab09d00635655
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-02-17 10:13:44 -0500

    Added some props to the todolist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoList from './components/TodoList/TodoList';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoList/>
+        <TodoList tasks={["Generate react app", "Intall react-bootstrap", "Generate TodoList component", "Make component reactive", "Add progress bar"]}/>
       </header>
     </div>
   );
