0edc1086d4151c0f0a0174dcc3b5c83cce2ba9f8
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-03-07 23:25:38 -0500

    Added "recursive" task creation

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -6,22 +6,31 @@ import { Form, InputGroup, Button } from 'react-bootstrap';
 class Task extends React.Component {
   constructor(props){
     super(props)
+    console.log(props)
     this.state = {
       name: props.name,
       subtasks: []
     };
+    this.toggleTaskCreation = this.toggleTaskCreation.bind(this);
   }
   render() {
-    return <Form.Group control-id={`task-${this.state.name.replace(' ', '-').toLowerCase()}`}>
-        <InputGroup >
-          <InputGroup.Checkbox variant="outline-secondary" type="checkbox" id="default-checkbox"/>
+    return <Form.Group className={this.props.className} control-id={`task-${this.state.name.replace(' ', '-').toLowerCase()}`}>
+        <InputGroup>
+          <InputGroup.Checkbox type="checkbox" id="default-checkbox basic-addon1"/>
           <InputGroup.Text className="flex-fill" variant="outline-secondary" id="inputGroup-sizing-default">{this.state.name}</InputGroup.Text>
-          <Button variant="outline-secondary" id="button-addon2">
+          <Button onClick={this.toggleTaskCreation} variant="outline-secondary" id="button-addon2">
             +
           </Button>
         </InputGroup>
+        {this.state.subtasks.map((taskName, index)=>{
+          return <Task className="ms-3" name={taskName} key={index}/>
+        })}
       </Form.Group>
   };
+  toggleTaskCreation(event){
+    event.preventDefault();
+    this.setState((state) => ({subtasks: [...state.subtasks, "Default task"]}))
+  }
 }
 
 export default Task;
