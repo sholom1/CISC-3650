22bc9c360fb5141418fd01e1ad42d02796f13ee4
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-03-08 21:08:52 -0500

    tweaked app css and structure

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoList tasks={["Generate react app", "Intall react-bootstrap", "Generate TodoList component", "Make component reactive", "Add progress bar"]}/>
+        <h1>Sam Kessler CISC 3650 TodoList Lab</h1>
       </header>
+      <div className='App-body'>
+        <TodoList tasks={["Generate react app", "Intall react-bootstrap", "Generate TodoList component", "Make component reactive", "Add progress bar"]}/>
+      </div>
     </div>
   );
 }
