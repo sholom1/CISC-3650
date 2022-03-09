978c62ad671f89376dbdab2252e36a5e157ec80d
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-03-08 22:22:20 -0500

    Added date to task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -11,17 +11,20 @@ class Task extends React.Component {
     console.log(props)
     this.state = {
       renaming: false,
-      subtasks: []
+      subtasks: [],
+      isChecked: false
     };
     this.addNewTask = this.addNewTask.bind(this);
     this.toggleRenameTask = this.toggleRenameTask.bind(this);
     this.onRemoveTask = this.onRemoveTask.bind(this);
+    this.handleCompletion = this.handleCompletion.bind(this);
   }
   render() {
     return <Form.Group className={this.props.className + " my-1"} control-id={`task-${this.props.name.replace(' ', '-').toLowerCase()}`}>
         <InputGroup>
-          <InputGroup.Checkbox type="checkbox" id="default-checkbox basic-addon1"/>
+          <InputGroup.Checkbox onChange={this.handleCompletion} checked={this.props.forceCheck || this.state.isChecked} type="checkbox" id="default-checkbox basic-addon1"/>
           <FormControl className="flex-fill" variant="outline-secondary" id="inputGroup-sizing-default" value={this.props.name} readOnly={!this.state.renaming} onChange={this.props.onNameChange}/>
+          <FormControl className="flex-shrink" type="date" size="sm"/>
           <Button variant='primary' label="Rename task" onClick={this.toggleRenameTask}>
             <img src='/vector-pen.svg'/>
           </Button>
@@ -33,24 +36,25 @@ class Task extends React.Component {
           </Button>
         </InputGroup>
         {this.state.subtasks.map((task, index)=>{
-          return <Task className="ms-4" name={task.name} key={task.id} onDelete={(event) => this.onRemoveTask(event, index)}/>
+          return <Task forceCheck={this.state.isChecked} className="ms-4" name={task.name} key={task.id} onDelete={(event) => this.onRemoveTask(event, index)}/>
         })}
       </Form.Group>
   };
   addNewTask(event){
-    event.preventDefault();
     this.setState((state) => ({subtasks: [...state.subtasks, generateNewTask()]}))
   }
   toggleRenameTask(){
     this.setState(state => ({renaming: !state.renaming}));
   }
   onRemoveTask(event, index){
-    event.preventDefault();
     this.setState(state => {
       let reducedTasks = [...state.subtasks]
       reducedTasks.splice(index, 1);
       return {subtasks: [...reducedTasks]};
     })
+  }
+  handleCompletion(event){
+    this.setState(state => ({isChecked: !state.isChecked}))
   }
 }
 
